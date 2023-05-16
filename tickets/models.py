c0d23a52ad40dabd6499e5412c9cb9c721c8d0a3
Elmorsi5 <mohamedelmorse5@gmail.com>
2023-05-16 10:55:20 +0300

    Creating Models and Serializers

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    hall = models.CharField(max_length=10)
+    Movie = models.CharField(max_length=10)
+    date = models.DateField()
+
+class Guest(models.Model):
+    name = models.CharField(max_length=20)
+    mobile = models.IntegerField()
+
+class Reservation(models.Model):
+    Guest = models.ForeignKey(Guest,related_name='reservations' ,on_delete= models.CASCADE)
+    Movie = models.ForeignKey(Movie,related_name="reservations" , on_delete =models.CASCADE)
+
+
